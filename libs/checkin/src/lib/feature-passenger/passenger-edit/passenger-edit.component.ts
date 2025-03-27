0aa9bf760e775470da52327d527d2ce349f32a3e
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-03-27 13:07:29 +0100

    refactor: passenger resource

--- libs/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,18 +1,17 @@
 import { NgIf } from '@angular/common';
-import { Component, DestroyRef, effect, inject, Injector, input, numberAttribute, runInInjectionContext, signal } from '@angular/core';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { Component, DestroyRef, effect, inject, Injector, input, numberAttribute } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { validatePassengerStatus } from '../../util-validation';
 import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
-import { initialPassenger } from '../../logic-passenger';
-import { switchMap } from 'rxjs';
+import { validatePassengerStatus } from '../../util-validation';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-passenger-edit',
   imports: [
     NgIf,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './passenger-edit.component.html'
 })
@@ -32,33 +31,21 @@ export class PassengerEditComponent {
   });
 
   id = input(0, { transform: numberAttribute });
-  passenger = toSignal(
-    toObservable(this.id).pipe(
-      switchMap(id => this.passengerService.findById(id))
-    ), { initialValue: initialPassenger }
-  );
+  passengerResource = this.passengerService.findByIdAsResource(this.id);
 
   constructor() {
-    effect(() => this.editForm.patchValue(
-      this.passenger()
-    ));
+    effect(() => {
+      const passenger = this.passengerResource.value();
+
+      if (passenger) {
+        this.editForm.patchValue(passenger);
+      }
+    });
 
     this.destroyRef.onDestroy(() => console.log('Bye, bye! :('));
   }
 
   protected save(): void {
     console.log(this.editForm.value);
-    runInInjectionContext(
-      this.injector,
-      () => effect(() => this.editForm.patchValue(
-        this.passenger()
-      ))
-    );
-
-    effect(() => this.editForm.patchValue(
-      this.passenger()
-    ), {
-      injector: this.injector
-    });    
   }
 }
