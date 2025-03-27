f07d8b8abb1ef3f63e355dd17a804ff580817139
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-03-27 08:59:52 +0100

    refactor: signal, computed, effect smart component

--- libs/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight, FlightFilter, injectTicketsFacade } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
+import { SIGNAL } from '@angular/core/primitives/signals';
 
 
 @Component({
@@ -18,25 +19,41 @@ import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
 export class FlightSearchComponent {
   private ticketsFacade = injectTicketsFacade();
 
-  protected filter = {
+  protected filter = signal({
     from: 'London',
     to: 'New York',
     urgent: false
-  };
+  });
+  protected route = computed(
+    () => 'From ' + this.filter().from + ' to ' + this.filter().to + '.'
+  );
   protected basket: Record<number, boolean> = {
     3: true,
     5: true
   };
   protected flights$ = this.ticketsFacade.flights$;
 
-  protected search(filter: FlightFilter): void {
-    this.filter = filter;
+  constructor() {
+    const reactiveFn = () => {
+      console.log(this.route());
+    };
 
-    if (!this.filter.from || !this.filter.to) {
+    const effectRef = effect(reactiveFn);
+    // effectRef.destroy();
+    // console.log(this.route[SIGNAL]);
+    // let activeConsumer = null;
+    // activeConsumer = effectRef
+
+  }
+
+  protected search(filter: FlightFilter): void {
+    this.filter.set(filter);
+
+    if (!this.filter().from || !this.filter().to) {
       return;
     }
 
-    this.ticketsFacade.search(this.filter);
+    this.ticketsFacade.search(this.filter());
   }
 
   protected delay(flight: Flight): void {
