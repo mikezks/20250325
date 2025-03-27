53d5d102891dbb18c1e18c5611744cec03a6f668
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-03-27 12:38:42 +0100

    fix: card template

--- libs/booking/src/lib/ui-flight/flight-card/flight-card.component.ts
+++ libs/booking/src/lib/ui-flight/flight-card/flight-card.component.ts
@@ -18,12 +18,12 @@ import { Flight } from '../../logic-flight';
       [ngStyle]="{ 'background-color': selected() ? 'rgb(204, 197, 185)' : 'white' }"
     >
       <div class="card-header">
-        <h2 class="card-title">{{ item()?.from }} - {{ item()?.to }}</h2>
+        <h2 class="card-title">{{ item().from }} - {{ item().to }}</h2>
       </div>
 
       <div class="card-body">
-        <p>Flight-No.: {{ item()?.id }}</p>
-        <p>Date: {{ item()?.date | date : "dd.MM.yyyy HH:mm" }}</p>
+        <p>Flight-No.: {{ item().id }}</p>
+        <p>Date: {{ item().date | date : "dd.MM.yyyy HH:mm" }}</p>
         <p>
           <button
             (click)="toggleSelection()"
@@ -31,7 +31,7 @@ import { Flight } from '../../logic-flight';
             style="min-width: 85px; margin-right: 5px"
           >{{ selected() ? "Remove" : "Select" }}</button>
           <a
-            [routerLink]="['../edit', item()?.id]"
+            [routerLink]="['../edit', item().id]"
             class="btn btn-success btn-sm"
             style="min-width: 85px; margin-right: 5px"
           >Edit</a>
